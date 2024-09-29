c4fff4de9a6ed6737d84ff58cb54eeb4c978042b
Nick Venenga <nick@venenga.com>
2024-09-29 17:00:10 -0400

    Abstract volume manager interface
    Create a generic volume manager interface and move lvm management into
    its own package. This will allow adding additional volume managers
    (qcow2 files on zfs)

--- internal/os_util.go
+++ internal/os_util.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"k8s.io/klog/v2"
@@ -10,52 +9,7 @@ import (
 	"strings"
 )
 
-type LogicalVolume struct {
-	LogicalVolume string `json:"lv_name"`
-	VolumeGroup   string `json:"vg_name"`
-}
-
-type LvReport struct {
-	Report []LogicalVolume `json:"lv"`
-}
-
-type LvmReportList struct {
-	Reports []LvReport `json:"report"`
-}
-
-func getVolumeVolumeGroupMapping() map[string]string {
-	mapping := make(map[string]string)
-
-	stdout, err := exec.Command(
-		"lvs",
-		"-o", "vg_name,lv_name",
-		"--reportformat", "json",
-	).Output()
-
-	if err != nil {
-		klog.ErrorS(err, "failed to get logical volume info")
-		return mapping
-	}
-
-	var report LvmReportList
-	err = json.Unmarshal(stdout, &report)
-
-	if err != nil {
-		klog.ErrorS(err, "failed to unmarshal logical volume info")
-		return mapping
-	}
-
-	// There should only be a single report
-	for _, report := range report.Reports {
-		for _, lv := range report.Report {
-			mapping[lv.LogicalVolume] = lv.VolumeGroup
-		}
-	}
-
-	return mapping
-}
-
-func processOutput(cmd *exec.Cmd) (string, string, error) {
+func ProcessOutput(cmd *exec.Cmd) (string, string, error) {
 	klog.InfoS("processing output", "command", cmd.Args)
 	bytesOut, err := cmd.Output()
 	stdout := strings.TrimRight(string(bytesOut), "\n\t\r ")
@@ -69,7 +23,7 @@ func processOutput(cmd *exec.Cmd) (string, string, error) {
 	return stdout, stderr, err
 }
 
-func writeTempFile(suffix, contents string) (string, error) {
+func WriteTempFile(suffix, contents string) (string, error) {
 	file, err := os.CreateTemp(os.TempDir(), fmt.Sprintf("%s-", suffix))
 	if err != nil {
 		return "", err
